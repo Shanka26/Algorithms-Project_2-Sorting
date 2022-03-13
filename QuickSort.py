f90475915eeb86f2ec2d5b27024f1ea9e0ccaeaa
Shanka26 <47332795+Shanka26@users.noreply.github.com>
2022-03-13 18:25:45 -0500

    different partition methods

--- QuickSort.py
+++ QuickSort.py
@@ -1,8 +1,43 @@
-
-import sys
 def partition(start, end, array):
      
     # Initializing pivot's index to to the median of start and end
+    mid = int((start+end)/2)
+    pivot_index = int((mid+end)/2)
+    # print(pivot_index)
+    pivot = array[pivot_index]
+     
+    # This loop runs till start pointer crosses
+    # end pointer, and when it does we swap the
+    # pivot with element on end pointer
+    
+    while start < end:
+         
+        # Increment the start pointer till it finds an
+        # element greater than  pivot
+        while start < len(array) and array[start] <= pivot:
+            start += 1
+             
+        # Decrement the end pointer till it finds an
+        # element less than pivot
+        while array[end] > pivot:
+            end -= 1
+         
+        # If start and end have not crossed each other,
+        # swap the numbers on start and end
+        if(start < end):
+            array[start], array[end] = array[end], array[start]
+     
+    # Swap pivot element with element on end pointer.
+    # This puts pivot on its correct sorted place.
+    array[end], array[pivot_index] = array[pivot_index], array[end]
+    
+    # Returning end pointer to divide the array into 2
+    return end
+
+def partitionMid(start, end, array):
+     
+    # Initializing pivot's index to to the median of start and end
+    
     pivot_index = int((start+end)/2)
     # print(pivot_index)
     pivot = array[pivot_index]
@@ -49,12 +84,24 @@ def quick_sort(start, end, array):
         quick_sort(start, p - 1, array)
         quick_sort(p + 1, end, array)
 
-def sort(arr):
+def quick_sortMid(start, end, array):
+     
+    if (start < end):
+         
+        # p is partitioning index, array[p]
+        # is at right place
+        p = partitionMid(start, end, array)
+         
+        # Sort elements before partition
+        # and after partition
+        quick_sortMid(start, p - 1, array)
+        quick_sortMid(p + 1, end, array)
+        
+
+def sortEndPivot(arr):
     quick_sort(0, len(arr) - 1, arr)
 
-#ar = [ 10, 7, 8, 9, 1, 5,7,9,3,5,7 ]
-#print(ar)
-#sort(ar)
-#print(ar)
+def sort(arr):
+    quick_sortMid(0, len(arr) - 1, arr)
+
      
-# This code is contributed by Adnan Aliakbar
