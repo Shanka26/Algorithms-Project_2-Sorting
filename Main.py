c2fdf9eabd7cd7544b0b2ab1c3f4886f61556374
dhawth1 <89355765+dhawth1@users.noreply.github.com>
2022-03-13 21:33:44 -0500

    More work done.

--- Main.py
+++ Main.py
@@ -1,24 +1,37 @@
+import sys
+import copy
 import TestSuite
 import QuickSort
+import QuickSort_not_Optomized as Quick_n
+import QuickSort_It
 import BubbleSort
 import MergeSort
 import TimSort
 
-n = 10000
+sys.setrecursionlimit(5000)
 file_name = 'Test_Output.txt'
-test_data = TestSuite.generate_data(n)
-with open(file_name,'a') as f:
-    print('----------------Testing BubbleSort----------------\n', file=f)
-TestSuite.time_sort(n, BubbleSort.sort,test_data,file_name)
 
+n = 1000000 #run BubbleSort at size 100,000 to get conclusive data as it scales
+test_data = TestSuite.generate_data(n)
+
+#with open(file_name,'a') as f:
+#    print('----------------Testing BubbleSort----------------\n', file=f)
+#TestSuite.time_sort(n, BubbleSort.sort,test_data,file_name)
+
+run_data = copy.deepcopy(test_data)
 with open(file_name,'a') as f:
     print('----------------Testing MergeSort----------------\n', file=f)
-TestSuite.time_sort(n, MergeSort.sort,test_data,file_name)
+TestSuite.time_sort(n, MergeSort.sort,run_data,file_name)
 
+run_data = copy.deepcopy(test_data)
 with open(file_name,'a') as f:
     print('----------------Testing TimSort----------------\n', file=f)
-TestSuite.time_sort(n, TimSort.timSort,test_data,file_name)
+TestSuite.time_sort(n, TimSort.timSort,run_data,file_name)
 
+run_data = copy.deepcopy(test_data)
 with open(file_name,'a') as f:
     print('----------------Testing QuickSort----------------\n', file=f)
-TestSuite.time_sort(n, QuickSort.sort,test_data,file_name)
+TestSuite.time_sort(n, QuickSort_It.sort,run_data,file_name)
+
+with open(file_name,'a') as f:
+    print('------------------------------------------------\n----------------Test Complete------------------\n------------------------------------------------', file=f)
